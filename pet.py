a85b771953b8ce616586be8565f14241b0e623d8
Ziang29b <Ziangc2@nycstudents.net>
2025-12-03 09:26:13 -0500

    hgnbv

--- pet.py
+++ pet.py
@@ -11,11 +11,11 @@ class Pet:
         self.happiness = happiness
     
     def buy(self, item):
-        cost = item["cost"]
+        price = item["price"]
         print ("do you want to buy?")
         if input == ("yes, yep, yea, yeah").lower:
-            if self.money >= cost:
-                self.money -= cost
+            if self.money >= price:
+                self.money -= price
             self.inventory.append(item)
             print("u bought:", item)
             print("heres your updated inventory:", self.inventory)
@@ -25,6 +25,48 @@ class Pet:
         else:
             print("ur too poor need more money", item["title"])
 dolphin = Pet("dolphin", 167, ["fish"], 100, 100, 100, 100, 100, 67)
-item = {"title": "t-90m", "cost": 67}
+stuff=[ 
+                {
+                "price":2,
+               "size" : "about 3 inches", 
+               "name": "apple"
+               },
+               {
+                "price": 2,
+                "size": "6 or 7 inches",
+                "name" : "banana"
+                },
+                {
+                "price" : 222, 
+                 "size": "10 feet long 1 feet 6 inches wide", 
+                 "name": "giant baguette pillow"
+                }
+                ]
+
+shopping = True 
+bought=[]
+while shopping:
+        user_input = input("what do you want, banana apple or giant baguette pillow: ").lower()
+        if user_input in [item["name"] for item in stuff]:
+             for item in stuff:
+                if item["name"] == user_input:
+                    print(item)
+                    bought.append (item)
+                
+
+
+        else: 
+            print("we no have")
+        user_input = input ("are you still shopping?").lower()
+        if user_input in ["yea" , "yes", "yeah", "yep" , "yuh"]:
+            print ("okay enjoy your shopping")
+        else: 
+            print ("cashier is up front",bought)
+            total = 0
+            for item in bought:
+                price_str = item["price"][0]
+                total += float(price_str)   
+            print("total price: $", total)
+            break 
 dolphin.buy(item)
 print(dolphin.__dict__)
