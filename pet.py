20f62013961726930636283c7b7649bac82d826e
Ziang29b <Ziangc2@nycstudents.net>
2025-12-16 09:12:18 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -11,14 +11,14 @@ class Pet:
         self.happiness = happiness
 
     def buy(self, item):
-        price = item["price"]                     
+        price = item["price"]                    
         print("do you want to buy?")
         answer = input("do you want to buy? ").lower()
 
         if answer in ("yes", "yep", "yea", "yeah"):
             if self.money >= price:
                 self.money -= price
-                self.inventory.append(item)       
+                self.inventory.append(item)      
                 print("u bought:", item)
                 print("heres your updated inventory:", self.inventory)
                 print("your money left:", self.money)
@@ -28,53 +28,49 @@ class Pet:
         elif answer in ("no", "nah", "nuh uh").lower():
             print("okay bye bye")
 
+
 dolphin = Pet("dolphin", 167, ["fish"], 100, 100, 100, 100, 100, 67)
 
 stuff = [
     {
-        "price": 2,                               
+        "price": 2,                              
         "size": "about 3 inches",
         "name": "apple"
     },
     {
-        "price": 2,                               
+        "price": 2,                              
         "size": "6 or 7 inches",
         "name": "banana"
     },
     {
-        "price": 222,                             
+        "price": 222,                            
         "size": "10 feet long 1 feet 6 inches wide",
         "name": "giant baguette pillow"
     }
 ]
 
 shopping = True
-bought = []
 
 while shopping:
     user_input = input("what do you want, banana apple or giant baguette pillow: ").lower()
 
-    if user_input in [item["name"] for item in stuff]:
-        for item in stuff:
-            if item["name"] == user_input:
-                print(item)
-                bought.append(item)
+    found = False
+    for item in stuff:
+        if item["name"] == user_input:
+            print(item)
+            dolphin.buy(item)   
+            found = True
+            break
 
-    else:
+    if not found:
         print("we no have")
 
     user_input = input("are you still shopping? ").lower()
-    if user_input in ["yea", "yes", "yeah", "yep", "yuh"]:
+    if user_input in ["yea", "yes", "yeah", "yep", "yuh"].lower():
         print("okay enjoy your shopping")
     else:
-        print("cashier is up front", bought)
-        total = 0
-        for item in bought:
-            total += item["price"]                
-        print("total price: $", total)
+        print("cashier is up front")
         break
 
-for item in bought:                               
-    dolphin.buy(item)
-
 print(dolphin.__dict__)
+
