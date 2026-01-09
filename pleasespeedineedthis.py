719ceb50621ab2188477bae0c973070d3d47cf7f
Ziang29b <Ziangc2@nycstudents.net>
2026-01-09 09:22:04 -0500

    Delete pleasespeedineedthis.py

--- pleasespeedineedthis.py
+++
@@ -1,5 +0,0 @@
-print ("oh my god im playing to lose brooooo, PLEASE SPEED I NEED THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS........ my mom is kinda homeless")
-if input == ("yo speed im watching your stream right now why you trying not to laugh bruh thats disrespectful as shii"):
-    print ("-V-")
-else:
-    print (" :}")
